e4ea0aac03a2017db527098c220a371342413856
gigsters-app <admin@gigsters.app>
2025-04-06 17:38:23 +0400

    config aws credentials

--- src/common/mail/mail.service.ts
+++ src/common/mail/mail.service.ts
@@ -7,9 +7,9 @@ export class MailService {
   private sesClient: SESClient;
 
   constructor(private readonly configService: ConfigService) {
-    const accessKeyId = this.configService.get('AWS_ACCESS_KEY_ID');
-    const secretAccessKey = this.configService.get('AWS_SECRET_ACCESS_KEY');
-    const region = this.configService.get('AWS_REGION');
+    const accessKeyId =  process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+    const region = process.env.AWS_REGION;
 
     if (!accessKeyId || !secretAccessKey || !region) {
       throw new Error('Missing AWS credentials');
